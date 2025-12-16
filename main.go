07b35e95b7e6f5dd6a7476b5f91aa8448bdb5e84
superbigerman <aleksejholmov1@gmail.com>
2025-12-16 23:31:59 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,39 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	service "test-project/ser"
+	"log"
+	"os"
+
+	"project/internal/service"
 )
 
-type Producer interface {
-	Produce() ([]string, error)
-}
-
-type Presenter interface {
-	Present(data []string)
-}
-
 func main() {
-	var p Producer = producer{}
+	// Аргументы командной строки
+	inputFile := flag.String("input", "", "Путь к входному файлу")
+	outputFile := flag.String("output", "output.txt", "Путь к выходному файлу")
+	flag.Parse()
 
-	var pr Presenter = presenter{}
-
-	dataservice := service.NewDataService(p, pr)
-	dataservice.Process()
-
-}
-
-type producer struct{}
-type presenter struct{}
-
-func (producer) Produce() ([]string, error) {
-	return []string{"яблоко", "банан", "апельсин"}, nil
-
-}
-
-func (presenter) Present(data []string) {
-	for i, item := range data {
-		fmt.Printf("%d. %s\n", i+1, item)
-
+	// Проверка обязательного параметра
+	if *inputFile == "" {
+		fmt.Println("Использование: program -input <входной_файл> [-output <выходной_файл>]")
+		fmt.Println("Пример: program -input data.txt -output result.txt")
+		fmt.Println("По умолчанию выходной файл: output.txt")
+		os.Exit(1)
 	}
+
+	// Проверяем существование входного файла
+	if _, err := os.Stat(*inputFile); os.IsNotExist(err) {
+		log.Fatalf("Файл %s не найден", *inputFile)
+	}
+
+	// Создаем продюсера и презентера
+	producer := service.NewFileProducer(*inputFile)
+	presenter := service.NewFilePresenter(*outputFile)
+
+	// Создаем и запускаем сервис
+	svc := service.NewService(producer, presenter)
+
+	if err := svc.Run(); err != nil {
+		log.Fatalf("Ошибка: %v", err)
+	}
+
+	fmt.Printf("Готово! Результат записан в %s\n", *outputFile)
 }
--- service.go
+++ service.go
@@ -1,24 +1,125 @@
 package service
 
-type DataService struct {
-	producer  interface{ Produce() ([]string, error) }
-	presenter interface{ Present(data []string) }
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+// Producer интерфейс для поставщика данных
+type Producer interface {
+	Produce() ([]string, error)
 }
 
-func NewDataService(producer interface{ Produce() ([]string, error) },
-	presenter interface{ Present(data []string) }) *DataService {
-	return &DataService{
-		producer:  producer,
-		presenter: presenter,
+// Presenter интерфейс для вывода результатов
+type Presenter interface {
+	Present([]string) error
+}
+
+// Service основной сервис
+type Service struct {
+	prod Producer
+	pres Presenter
+}
+
+// NewService создает новый сервис
+func NewService(prod Producer, pres Presenter) *Service {
+	return &Service{
+		prod: prod,
+		pres: pres,
 	}
 }
 
-func (s *DataService) Process() error {
-	data, err := s.producer.Produce()
+// Run запускает обработку данных
+func (s *Service) Run() error {
+	// Получаем данные
+	data, err := s.prod.Produce()
 	if err != nil {
 		return err
 	}
-	s.presenter.Present(data)
-	return nil
 
+	// Обрабатываем данные
+	result := s.MaskData(data)
+
+	// Выводим результат
+	return s.pres.Present(result)
+}
+
+// MaskData маскирует данные
+func (s *Service) MaskData(data []string) []string {
+	var result []string
+
+	for _, line := range data {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		words := strings.Fields(line)
+		maskedWords := make([]string, len(words))
+
+		for i, word := range words {
+			if len(word) > 1 {
+				maskedWords[i] = string(word[0]) + strings.Repeat("*", len(word)-1)
+			} else {
+				maskedWords[i] = word
+			}
+		}
+
+		result = append(result, strings.Join(maskedWords, " "))
+	}
+
+	return result
+}
+
+// FileProducer читает данные из файла
+type FileProducer struct {
+	FilePath string
+}
+
+// NewFileProducer создает новый FileProducer
+func NewFileProducer(filePath string) *FileProducer {
+	return &FileProducer{FilePath: filePath}
+}
+
+// Produce читает строки из файла
+func (fp *FileProducer) Produce() ([]string, error) {
+	file, err := os.Open(fp.FilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+// FilePresenter записывает данные в файл
+type FilePresenter struct {
+	FilePath string
+}
+
+// NewFilePresenter создает новый FilePresenter
+func NewFilePresenter(filePath string) *FilePresenter {
+	return &FilePresenter{FilePath: filePath}
+}
+
+// Present записывает данные в файл
+func (fp *FilePresenter) Present(data []string) error {
+	content := strings.Join(data, "\n")
+
+	file, err := os.Create(fp.FilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	return err
 }
